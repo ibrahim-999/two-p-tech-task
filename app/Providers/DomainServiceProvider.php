cf78c001f7800cd37068ba4c4e94f6ad5da4434a
ibrahim <ibrahim.khalaf99@gmail.com>
2025-05-25 16:05:58 +0300

    bind Repos

---
+++ app/Providers/DomainServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use App\Domains\Cart\Repositories\CartRepositoryInterface;
+use App\Domains\Order\Repositories\OrderRepositoryInterface;
+use App\Domains\Product\Repositories\ProductRepositoryInterface;
+use App\Infrastructure\Repositories\CartRepository;
+use App\Infrastructure\Repositories\OrderRepository;
+use App\Infrastructure\Repositories\ProductRepository;
+use Illuminate\Support\ServiceProvider;
+
+class DomainServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
+        $this->app->bind(CartRepositoryInterface::class, CartRepository::class);
+        $this->app->bind(OrderRepositoryInterface::class, OrderRepository::class);
+    }
+
+    public function boot()
+    {
+    }
+
+}
