5d7cd1f913af93bad751fc1fe7a3a572e69712e1
ibrahim <ibrahim.khalaf99@gmail.com>
2025-05-25 15:35:03 +0300

    moved user model

---
+++ app/Domains/User/Models/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Domains\User\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Authenticatable
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    public function cart(): HasOne
+    {
+        return $this->hasOne(Cart::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
+}
